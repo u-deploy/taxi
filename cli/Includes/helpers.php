5f00ec8eb8353ba8b84947d51102359559d21ca1
Richard Styles <richardstyles@gmail.com>
2023-03-22 21:11:29 +0000

    setup helpers

---
+++ cli/Includes/helpers.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Taxi;
+
+use function Valet\testing;
+
+if (! defined('TAXI_HOME_PATH')) {
+    if (testing()) {
+        define('TAXI_HOME_PATH', __DIR__.'/../../tests/config/taxi');
+    } else {
+        define('TAXI_HOME_PATH', $_SERVER['HOME'].'/.config/taxi');
+    }
+}
+
+function git_branch(string $sitePath): string
+{
+    if (!file_exists($sitePath . '/.git/HEAD')) {
+        return '';
+    }
+
+    return implode('/', array_slice(explode('/', file_get_contents($sitePath . '/.git/HEAD')), 2));
+}
