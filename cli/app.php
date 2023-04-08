ac3c888d924db78aa424ac83a94b20477339e2e8
Richard Styles <richardstyles@gmail.com>
2023-04-08 21:16:01 +0100

    Initial version

--- cli/app.php
+++ cli/app.php
@@ -12,7 +12,7 @@ use function Valet\table;
 use function Valet\warning;
 use function Valet\writer;
 
-$version = '0.0.0';
+$version = '0.0.1';
 
 /**
  * Load correct autoloader depending on install location.
