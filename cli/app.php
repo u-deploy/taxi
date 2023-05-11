03daf208c440d51523bca46a2bc32d1f42676566
Richard Styles <richardstyles@gmail.com>
2023-05-11 20:52:51 +0100

    version bump

--- cli/app.php
+++ cli/app.php
@@ -13,7 +13,7 @@ use function Valet\table;
 use function Valet\warning;
 use function Valet\writer;
 
-$version = '0.0.2';
+$version = '0.0.4';
 
 /**
  * Load correct autoloader depending on install location.
